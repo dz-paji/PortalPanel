bd023d65afb0b8863bef47ee55d7a98fa3815cbb
dz-paji <zhangqian2002@outlook.com>
2019-05-08 21:01:57 +0800

    format html

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,67 +17,68 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.loli.net/icon?family=Material+Icons" rel="stylesheet">
-    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    <link href="https://fonts.loli.net/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="app">
-    <nav class="nav-wrapper blue-grey  lighten-3 ">
+        <nav class="nav-wrapper blue-grey  lighten-3 ">
             <div class="container">
                 <a class="navbar-brand " href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul id="nav-mobile" class="right hide-on-med-and-down">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li >
-                                <a href="{{ route('login') }}" >{{ __('登陆') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li>
-                                    <a href="{{ route('register') }}">{{ __('注册') }}</a>
-                                </li>
-                            @endif
-                        @else
-                        <ul id="dropdown1" class="dropdown-content">
+                <!-- Right Side Of Navbar -->
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <!-- Authentication Links -->
+                    @guest
+                    <li>
+                        <a href="{{ route('login') }}">{{ __('登陆') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                    <li>
+                        <a href="{{ route('register') }}">{{ __('注册') }}</a>
+                    </li>
+                    @endif
+                    @else
+                    <ul id="dropdown1" class="dropdown-content">
                         <li><a href="profile">我的资料</a></li>
                         <li class="divider"></li>
-                        <li><a href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
+                        <li><a href="{{ route('logout') }}" onclick="event.preventDefault();
                             document.getElementById('logout-form').submit();">
-                            登出
-                         </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                        </form>
+                                登出
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
                         </li>
-                        </ul>
-                            <li>
-                                <a class="dropdown-trigger" href="#!" data-target="dropdown1">{{ Auth::user()->name }}<i class="material-icons right">arrow_drop_down</i></a>
-                            </li>
-                        @endguest
                     </ul>
-                </div>
+                    <li>
+                        <a class="dropdown-trigger" href="#!" data-target="dropdown1">{{ Auth::user()->name }}<i class="material-icons right">arrow_drop_down</i></a>
+                    </li>
+                    @endguest
+                </ul>
             </div>
-        </nav>
+    </div>
+    </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+    <main class="py-4">
+        @yield('content')
+    </main>
     </div>
 </body>
 <footer class="page-footer wine-red">
-<div class="footer-copyright">
-      <div class="container">
-      <a class="brown-text text-lighten-3" href="http://www.miitbeian.gov.cn/">冀ICP备17022988-号3</a>
-      <p>Powered by <a class="brown-text text-lighten-3" href="https://github.com/dz-paji/PortalPanel">PortalPanel</a></p>
-      </div>
+    <div class="footer-copyright">
+        <div class="container">
+            <a class="brown-text text-lighten-3" href="http://www.miitbeian.gov.cn/">冀ICP备17022988-号3</a>
+            <p>Powered by <a class="brown-text text-lighten-3" href="https://github.com/dz-paji/PortalPanel">PortalPanel</a></p>
+        </div>
     </div>
 </footer>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>MicroDistance</title>
 
   <!-- CSS  -->
   <link href="https://fonts.loli.net/icon?family=Material+Icons" rel="stylesheet">
-  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="css/app.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
 </head>
+
 <body>
   <nav class="white" role="navigation">
     <div class="nav-wrapper container">
@@ -32,13 +32,11 @@
         <br><br>
         <h1 class="header center teal-text text-lighten-6">微距教育</h1>
         <div class="row center">
-          <h5 class="header col s12 light">华北地区首个高中生自主创办的国际青年论坛</h5>
+          <h5 class="header col s12 light red-text text-lighten-5">华北地区首个高中生自主创办的国际青年论坛</h5>
         </div>
-        <div class="row center">
+        <div class="row center center-align col s10 offset-s1">
           <a href="register" id="download-button" class="btn-large waves-effect waves-light wine-red lighten-1">即刻报名</a>
         </div>
-        <br><br>
-
       </div>
     </div>
     <div class="parallax"><img src="background1.jpg" alt="Unsplashed background img 1"></div>
@@ -94,18 +92,19 @@
   </div>
 
   <div class="row">
-        <div class="col s12 center">
-          <h3><i class="mdi-content-send brown-text"></i></h3>
-          <h4>更多文章</h4>
-          <p class=" light">想要了解更多关于微距的历史？想知道有谁参与了微距的活动？欢迎点击下方按钮进入我们的新闻站</p>
-          <div class="row center">
-              <a href="#" id="download-button" class="btn-large waves-effect waves-light wine-red lighten-1">新闻站</a>
-            </div>
-
-        </div>
+    <div class="col s12 center">
+      <h3><i class="mdi-content-send brown-text"></i></h3>
+      <h4>更多文章</h4>
+      <p class=" light">想要了解更多关于微距的历史？想知道有谁参与了微距的活动？欢迎点击下方按钮进入我们的新闻站</p>
+      <div class="row center">
+        <a href="#" id="download-button" class="waves-effect btn-large waves-light wine-red lighten-1">新闻站</a>
       </div>
 
+
     </div>
+  </div>
+
+  </div>
   </div>
 
   <div class="parallax-container valign-wrapper">
@@ -128,9 +127,9 @@
           <h4>联系我们</h4>
           <p class=" light">您可以在本网站右下角联系我们的在线客服，或点击下方按钮，选择更多联系途径。</p>
           <div class="row center">
-              <a href="#" id="download-button" class="btn-large waves-effect waves-light wine-red lighten-1">微信公众号</a>
-              <a href="#" id="download-button" class="btn-large waves-effect waves-light wine-red lighten-1">发送邮件</a>
-            </div>
+            <a href="#" id="download-button" class="btn-large waves-effect waves-light wine-red lighten-1">微信公众号</a>
+            <a href="#" id="download-button" class="btn-large waves-effect waves-light wine-red lighten-1">发送邮件</a>
+          </div>
 
         </div>
       </div>
@@ -157,8 +156,8 @@
     </div>
     <div class="footer-copyright">
       <div class="container">
-      <a class="brown-text text-lighten-3" href="http://www.miitbeian.gov.cn/">冀ICP备17022988-号3</a>
-      <p>Powered by <a class="brown-text text-lighten-3" href="https://github.com/dz-paji/PortalPanel">PortalPanel</a></p>
+        <a class="brown-text text-lighten-3" href="http://www.miitbeian.gov.cn/">冀ICP备17022988-号3</a>
+        <p>Powered by <a class="brown-text text-lighten-3" href="https://github.com/dz-paji/PortalPanel">PortalPanel</a></p>
       </div>
     </div>
   </footer>
@@ -169,5 +168,6 @@
   <script src="js/materialize.js"></script>
   <script src="js/init.js"></script>
 
-  </body>
+</body>
+
 </html>
