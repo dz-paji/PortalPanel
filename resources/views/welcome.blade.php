beff058dc034439ad21b54d97e9825a373dd1341
dz_paji <zhangqian2002@outlook.com>
2019-05-28 10:36:02 +0800

    finish wechat modal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -96,9 +96,9 @@
     <div class="col s12 center">
       <h3><i class="mdi-content-send brown-text"></i></h3>
       <h4>更多文章</h4>
-      <p class="light">想要了解更多关于微距的历史？想知道有谁参与了微距的活动？欢迎点击下方按钮进入我们的新闻站</p>
+      <p class="light">想要了解更多关于微距的历史？想知道有谁参与了微距的活动？欢迎点击下方按钮进入我们的微信公众号</p>
       <div class="row center">
-        <a href="#" id="download-button" class="waves-effect btn-large waves-light wine-red lighten-1">新闻站</a>
+        <a href="#" id="download-button" class="waves-effect btn-large waves-light wine-red lighten-1">查看微信公众号</a>
       </div>
     </div>
   </div>
@@ -125,7 +125,7 @@
           <h4>联系我们</h4>
           <p class=" light">您可以在本网站右下角联系我们的在线客服，或点击下方按钮，选择更多联系途径。</p>
           <div class="row center">
-            <a onclick="openModal1()" id="download-button" class="btn-large waves-effect waves-light wine-red lighten-1">微信公众号</a>
+            <a onclick="$('#modal1').modal('open');" id="download-button" class="btn-large waves-effect waves-light wine-red lighten-1">微信公众号</a>
             <a id="download-button" class="btn-large waves-effect waves-light wine-red lighten-1">发送邮件</a>
           </div>
         </div>
@@ -145,20 +145,10 @@
         <div class="col l3 s12">
           <h5 class="white-text">更多链接</h5>
           <ul>
-            <li><a class="waves-effect waves-light white-text" onclick="openModal1()">微信公众号</a></li>
+            <li><a class="waves-effect waves-light white-text" onclick="$('#modal1').modal('open');">微信公众号</a></li>
           </ul>
         </div>
       </div>
-      <div id="modal1" class="modal">
-            <div class="modal-content">
-              <div id="modal_content" class="row">
-
-              </div>
-              <div class="modal-footer">
-                <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Ok</a>
-              </div>
-            </div>
-          </div>
       <div class="footer-copyright">
         <div class="container">
           <a class="brown-text text-lighten-3" href="http://www.miitbeian.gov.cn/">冀ICP备17022988-号3</a>
@@ -168,12 +158,22 @@
       </div>
   </footer>
 
+  <div id="modal1" class="modal odal-fixed-footer">
+    <div class="modal-content center-align">
+      <img width="260px" height="260px" src="https://i.loli.net/2019/05/10/5cd538ad78caa.png">
+      <h6>请扫描上方二维码关注我们</h6>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">关闭</a>
+    </div>
+  </div>
+
 
   <!--  Scripts-->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="js/materialize.js"></script>
   <script src="js/init.js"></script>
-  <script src="resources/interReact.js"></script>
+  <script src="js/interReact.js"></script>
 
 </body>
 
